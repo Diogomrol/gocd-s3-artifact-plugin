762e344ac912c51f558873912624d4d831fe1cc0
jeevansai <jeevansai502@gmail.com>
2022-09-08 18:41:52 +0530

    Modify getFilesMatchingPattern in AntDirectoryScanner to return only files
    Co-authored-by: Raghav Gupta <raghav.gupta@thoughtworks.com>
    Co-authored-by: Krishnaswamy Subramaniam <jskswamy@gmail.com>

--- src/main/java/diogomrol/gocd/s3/artifact/plugin/model/AntDirectoryScanner.java
+++ src/main/java/diogomrol/gocd/s3/artifact/plugin/model/AntDirectoryScanner.java
@@ -22,14 +22,16 @@ public class AntDirectoryScanner {
             File[] files = new File(baseDir, directory).listFiles();
             if(files != null) {
                 for(File f : files) {
-                    allFiles.add(new File(directory, f.getName()));
+                    if (!f.isDirectory()) {
+                        allFiles.add(new File(directory, f.getName()));
+                    }
                 }
             }
         }
 
         for (int i = 0; i < allPaths.length; i++) {
             File file = new File(allPaths[i]);
-            if (!allFiles.contains(file)) {
+            if ((!allFiles.contains(file)) && (!file.isDirectory())) {
                 allFiles.add(new File(allPaths[i]));
             }
         }
